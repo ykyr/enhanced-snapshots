6fdf77c49c6c5998f2f23c1867a21009add0846b
Nik Weidenbacher <nik.weidenbacher@sungardas.com>
2015-06-26 01:40:57 +0000

    bootstrap.sh tweaks

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,27 @@
-sudo yum install -y git s3cmd --enablerepo=epel
-
-wget http://www.opendedup.org/downloads/SDFS-2.0.11-2.x86_64.rpm
-rpm --force -iv SDFS-2.0.11-2.x86_64.rpm
+#wget http://www.opendedup.org/downloads/SDFS-2.0.11-2.x86_64.rpm
+#rpm --force -iv SDFS-2.0.11-2.x86_64.rpm
 
 aws_account_id=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep accountId | cut -f2 -d: | cut -f2 -d\"`
 
-bucketname = "$aws_account_id-snapdirector-$SNAPDIRECTORNAME"
+bucketname="$aws_account_id-snapdirector-$SNAPDIRECTORNAME"
 
 mkdir /opt/sdfs
 aws s3 sync s3://$bucketname/opt/sdfs /opt/sdfs
 if [ -d /opt/sdfs/volumes/s3backed0 ] ; then
+  echo "Existing sdfs found on S3"
   aws s3 sync s3://$bucketname/etc/sdfs /etc/sdfs
   aws s3 sync s3://$bucketname/var/log/sdfs /var/log/sdfs
 else
-	mkfs.sdfs\
-		--volume-name=s3backed0 \
-		--volume-capacity=256TB \
-		--aws-enabled=true \
-		--cloud-access-key=AKIAI5OLTIAOOMBMAW4Q \
-		--cloud-bucket-name=$bucketname \
-		--cloud-secret-key=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 \
-		--chunk-store-encrypt=true \
-		--aws-bucket-location=US
+  echo "No sdfs on S3, so create a local one"
+  mkfs.sdfs\
+    --volume-name=s3backed0 \
+    --volume-capacity=256TB \
+    --aws-enabled=true \
+    --cloud-access-key=AKIAI5OLTIAOOMBMAW4Q \
+    --cloud-bucket-name=$bucketname \
+    --cloud-secret-key=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 \
+    --chunk-store-encrypt=true \
+    --aws-bucket-location=US
 fi
 
 mkdir -p /media/s3backed0
