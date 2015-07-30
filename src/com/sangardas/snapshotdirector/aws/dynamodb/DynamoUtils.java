6c54ce489726acbedffdf78fb15a9c3e1edd89ed
Dmitriy Vasiliev <dvas@softserveinc.com>
2015-07-30 17:42:46 +0300

    Implemented: - user authentication and user info retreiving methods in DynamoUtils
    Changed:
    - ORM class of Users table. Added attributes and indexes. Removed
    attribute 'Id'

--- src/com/sangardas/snapshotdirector/aws/dynamodb/DynamoUtils.java
+++ src/com/sangardas/snapshotdirector/aws/dynamodb/DynamoUtils.java
@@ -2,6 +2,8 @@ package com.sangardas.snapshotdirector.aws.dynamodb;
 
 import java.util.List;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
@@ -9,20 +11,27 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 import com.amazonaws.services.ec2.model.Volume;
 import com.sangardas.snapshotdirector.aws.EnvironmentBasedCredentialsProvider;
 import com.sangardas.snapshotdirector.aws.dynamodb.model.BackupEntry;
+import com.sangardas.snapshotdirector.aws.dynamodb.model.User;
+
 /**
- * DynamoUtils is a Repository for static methods which manipulate data in DynamoDB </br>
+ * DynamoUtils is a Repository for static methods which manipulate data in
+ * DynamoDB </br>
  * 
  * @author dvas
  *
  */
 public class DynamoUtils {
-	
+
 	/**
-	 * Returns the list of {@link BackupEntry} objects which represent records in DB.
-	 * @param volumeId {@link String}
-	 * @return {@link List} 
+	 * Returns the list of {@link BackupEntry} objects which represent records
+	 * in DB.
+	 * 
+	 * @param volumeId
+	 *            {@link String}
+	 * @return {@link List}
 	 */
-	public static List<BackupEntry> getBackupInfo(String volumeId, DynamoDBMapper mapper) {
+	public static List<BackupEntry> getBackupInfo(String volumeId,
+			DynamoDBMapper mapper) {
 
 		BackupEntry backupEntry = new BackupEntry();
 		backupEntry.setVolumeId(volumeId);
@@ -36,22 +45,59 @@ public class DynamoUtils {
 		return backupEntries;
 
 	}
-	
-	public static List<BackupEntry> getBackupInfo(Volume volume, DynamoDBMapper mapper){
+
+	public static List<BackupEntry> getBackupInfo(Volume volume,
+			DynamoDBMapper mapper) {
 		return getBackupInfo(volume.getVolumeId(), mapper);
 	}
-	
-	public void putBackupInfo(List<BackupEntry> bakupEntries, DynamoDBMapper mapper){
-		//TODO: Overload this method
-		
-		DynamoDBMapperConfig config = new DynamoDBMapperConfig(DynamoDBMapperConfig.SaveBehavior.CLOBBER);
+
+	public void putBackupInfo(List<BackupEntry> bakupEntries,
+			DynamoDBMapper mapper) {
+		// TODO: Overload this method
+
+		DynamoDBMapperConfig config = new DynamoDBMapperConfig(
+				DynamoDBMapperConfig.SaveBehavior.CLOBBER);
 		mapper.batchWrite(bakupEntries, null, config);
-		
-		
+
 	}
-	
-	public void removeBackupInfo(){
-		//TODO: Implement removeBackupInfo
+
+	public void removeBackupInfo() {
+		// TODO: Implement removeBackupInfo
+	}
+
+	public static boolean authenticateUser(String email, String pass,
+			DynamoDBMapper mapper) {
+		User user = getUser(email, mapper);
+		if (user.getPassword().equals(getPasswordHash(pass))) {
+			return true;
+		}
+
+		return false;
+
+	}
+
+	public static String getFullUserInfoByEmail(String email, DynamoDBMapper mapper) {
+		return getUser(email, mapper).getUserInfo();
+	}
+
+	private static User getUser(String email, DynamoDBMapper mapper) {
+		User user = new User();
+		user.setEmail(email);
+		DynamoDBQueryExpression<User> expression = new DynamoDBQueryExpression<User>()
+				.withHashKeyValues(user)
+				.withConsistentRead(false);
+
+		List<User> userEntries = mapper.query(User.class, expression);
+
+		if (userEntries == null || userEntries.isEmpty()) {
+			return null;
+		}
+
+		return userEntries.get(0);
+	}
+
+	private static String getPasswordHash(String pass) {
+		return DigestUtils.sha512Hex(pass);
 	}
 
 }
