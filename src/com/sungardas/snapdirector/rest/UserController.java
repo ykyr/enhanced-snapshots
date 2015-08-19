3ae2a86846ff7678058454b2e6ed59517f13f2cc
Iryna Radaikina <iradaik@softserveinc.com>
2015-08-19 10:55:48 +0300

    JIRA: SNAP-100, SNAP-101 Add support for user updating and user removing

--- src/com/sungardas/snapdirector/rest/UserController.java
+++ src/com/sungardas/snapdirector/rest/UserController.java
@@ -3,42 +3,101 @@ package com.sungardas.snapdirector.rest;
 import com.sungardas.snapdirector.dto.UserDto;
 import com.sungardas.snapdirector.exception.DataAccessException;
 import com.sungardas.snapdirector.exception.SnapdirectorException;
+import com.sungardas.snapdirector.rest.utils.Constants;
 import com.sungardas.snapdirector.service.UserService;
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.Collections;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import java.util.Map;
 
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private ServletContext context;
+	@Autowired
+	private HttpServletRequest servletRequest;
 
-    @Autowired
-    private UserService userService;
+	private ObjectMapper mapper;
 
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<String> createUser(@ModelAttribute UserDto userInfo, @RequestParam String password) {
-        try {
-            userService.createUser(userInfo, password);
-            return new ResponseEntity<>("User was successfully created.", HttpStatus.OK);
-        } catch (SnapdirectorException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<String> createUser(@RequestBody String userInfo) {
+		try {
+			// getting userDto from json
+			UserDto user = getUserDtoFromJson(userInfo);
+			// getting password
+			String password = mapper.readValue(userInfo, ObjectNode.class).get("password").asText();
+			userService.createUser(user, password, getCurrentUserEmail());
+			return new ResponseEntity<>("", HttpStatus.OK);
+		} catch (SnapdirectorException e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (IOException e) {
+			return new ResponseEntity<>("Failed to parse json", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@RequestMapping(method = RequestMethod.PUT)
+	public ResponseEntity updateUser(@RequestBody String userInfo) {
+		try {
+			// getting userDto from json
+			UserDto user = getUserDtoFromJson(userInfo);
+
+			// getting password
+			String password = mapper.readValue(userInfo, ObjectNode.class).get("password").asText();
+			userService.updateUser(user, password, getCurrentUserEmail());
+			return new ResponseEntity<>("", HttpStatus.OK);
+		} catch (SnapdirectorException e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (IOException e) {
+			return new ResponseEntity<>("Failed to parse json", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
 
-    @RequestMapping(method = GET)
-    public ResponseEntity getAllUsers(){
-        try {
-            return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
-        } catch (DataAccessException e){
-            return new ResponseEntity<>(Collections.emptyList(), HttpStatus.NOT_ACCEPTABLE);
-        }
-    }
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity getAllUsers() {
+		try {
+			return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+		} catch (DataAccessException e) {
+			return new ResponseEntity<>(Collections.emptyList(), HttpStatus.NOT_ACCEPTABLE);
+		}
+	}
+
+
+	@RequestMapping(value = "/{userEmail}", method = RequestMethod.DELETE)
+	public ResponseEntity removeUser(@PathVariable("userEmail") String userEmail) {
+		try {
+			userService.removeUser(userEmail, getCurrentUserEmail());
+			return new ResponseEntity<>("", HttpStatus.OK);
+		} catch (SnapdirectorException e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+
+	private String getCurrentUserEmail() {
+		String session = servletRequest.getSession().getId();
+		return ((Map<String, String>) context.getAttribute(Constants.CONTEXT_ALLOWED_SESSIONS_ATR_NAME)).get(session);
+	}
+
+	private UserDto getUserDtoFromJson(String json) throws IOException {
+		if (mapper == null) {
+			mapper = new ObjectMapper();
+			mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		}
+		return mapper.readValue(json, UserDto.class);
+	}
 }
--- src/com/sungardas/snapdirector/service/UserService.java
+++ src/com/sungardas/snapdirector/service/UserService.java
@@ -6,7 +6,11 @@ import com.sungardas.snapdirector.dto.UserDto;
 import java.util.List;
 
 public interface UserService {
-    List<UserDto> getAllUsers();
+	List<UserDto> getAllUsers();
 
-    void createUser(UserDto newUser, String password);
+	void createUser(UserDto newUser, String password, String currentUserEmail);
+
+	void updateUser(UserDto newUser, String newPassword, String currentUserEmail);
+
+	void removeUser(String userEmail, String currentUserEmail);
 }
