113a63c88a38c3c125e7652a80310ba98af46abd
Iryna Radaikina <iradaik@softserveinc.com>
2015-08-20 09:32:52 +0300

    JIRA: SNAP-101 Add additional check that not admin can not set admin role to himself while updating profile

---
+++ test/com/sungardas/snapdirector/service/impl/UserServiceImplTest.java
@@ -0,0 +1,54 @@
+package com.sungardas.snapdirector.service.impl;
+
+import org.junit.Test;
+
+
+public class UserServiceImplTest {
+
+	@Test
+	public void shouldCreateUserAccount(){
+		//TODO: implement
+	}
+
+	@Test
+	public void shouldCreateUserAccountForAdmin(){
+		//TODO: implement
+	}
+
+	@Test
+	public void shouldRemoveUserAccount(){
+		//TODO: implement
+	}
+
+	@Test
+	public void shouldRemoveUserAccountForAdmin(){
+		//TODO: implement
+	}
+
+	@Test
+	public void shouldUpdateUserAccount(){
+		//TODO: implement
+	}
+
+	@Test
+	public void shouldUpdateUserAccountForAdmin(){
+		//TODO: implement
+	}
+
+	@Test
+	public void shouldNotRemoveAdminUserInCaseItIsLastAdmin(){
+		//TODO: implement
+	}
+
+	@Test
+	public void shouldNotChangeAdminRoleInCaseItIsLastAdmin(){
+		//TODO: implement
+	}
+
+	@Test
+	public void shouldNotSetAdminRoleInCaseItIsUser(){
+		//TODO: implement
+	}
+
+
+}
