dbcbe72133011ef2b612035617d111bce0401ad2
Oleksii Goralik <ogor@softservecom.com>
2015-08-21 18:21:36 +0300

    SNAP-21: remove unused imports

--- src/com/sungardas/snapdirector/tasks/AWSBackupVolumeTask.java
+++ src/com/sungardas/snapdirector/tasks/AWSBackupVolumeTask.java
@@ -13,7 +13,6 @@ import com.sungardas.snapdirector.service.AWSCommunticationService;
 import com.sungardas.snapdirector.service.ConfigurationService;
 import com.sungardas.snapdirector.service.StorageService;
 import com.sungardas.snapdirector.tasks.aws.VolumeBackup;
-import com.sungardas.snapdirector.tasks.aws.sdfs.utils.SdfsManager;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -22,6 +21,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.io.IOException;
 
 import static com.sungardas.snapdirector.aws.dynamodb.model.TaskEntry.TaskEntryStatus.RUNNING;
@@ -70,13 +70,11 @@ public class AWSBackupVolumeTask implements BackupTask {
         taskEntry.setStatus(RUNNING.getStatus());
         taskRepository.save(taskEntry);
 		
-		SdfsManager sdfs = new SdfsManager(configuration);
-
 		Volume tempVolume = null;
 		String attachedDeviceName = null;
 		
 		tempVolume = VolumeBackup.createAndAttachBackupVolume(ec2client, volumeId, configuration.getConfigurationId());
-		attachedDeviceName = tempVolume.getAttachments().get(0).getDevice();
+		attachedDeviceName = detectFsDevName(tempVolume);
 
 		String backupDate = String.valueOf(System.currentTimeMillis());
 		String backupfileName = volumeId + "." + backupDate + ".backup";
@@ -96,6 +94,7 @@ public class AWSBackupVolumeTask implements BackupTask {
 			if (configuration.isUseFakeBackup()) {
 				source = configuration.getFakeBackupSource();
 			} else {
+				
 				source = attachedDeviceName;
 			}
 			LOG.info("Starting copying: " + source + " to:" +backupfileName);
@@ -129,6 +128,18 @@ public class AWSBackupVolumeTask implements BackupTask {
 		LOG.info("Task " + taskEntry.getId() + ": Delete completed task:" + taskEntry.getId());
         taskRepository.delete(taskEntry);
         LOG.info("Task completed.");
+	}
+	
+	private String detectFsDevName(Volume volume) {
+		String devname = volume.getAttachments().get(0).getDevice();
+		File volf = new File(devname);
+		if (!volf.exists() || !volf.isFile()) {
+			LOG.info(format("Cant find attached source: %s", volume));
+			
+			devname = "/dev/xvd" + devname.substring(devname.length()-1);
+			LOG.info(format("New sourcepash : %s", devname));
+		}
+		return devname;
 	}
 
 }
