272e1345263d26ff8d0b979d746d0936cf84d0db
Kostiantyn Glukhenko <k.glukhenko@levi9.com>
2015-08-31 15:19:34 +0300

    {SNAP-106] BE: Change model for errors

--- src/com/sungardas/snapdirector/rest/UserController.java
+++ src/com/sungardas/snapdirector/rest/UserController.java
@@ -1,28 +1,45 @@
 package com.sungardas.snapdirector.rest;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import com.sungardas.snapdirector.dto.ExceptionDto;
 import com.sungardas.snapdirector.dto.UserDto;
 import com.sungardas.snapdirector.exception.DataAccessException;
 import com.sungardas.snapdirector.exception.SnapdirectorException;
 import com.sungardas.snapdirector.rest.utils.Constants;
 import com.sungardas.snapdirector.service.UserService;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.codehaus.jackson.map.DeserializationConfig;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Map;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+
+	private static final Log LOG = LogFactory.getLog(UserController.class);
 
 	@Autowired
 	private UserService userService;
@@ -33,38 +50,43 @@ public class UserController {
 
 	private ObjectMapper mapper;
 
+	@ExceptionHandler(SnapdirectorException.class)
+	@ResponseBody
+	@ResponseStatus(INTERNAL_SERVER_ERROR)
+	private ExceptionDto internalServerError(SnapdirectorException exception){
+		LOG.error(exception);
+		return new ExceptionDto(INTERNAL_SERVER_ERROR.value(), exception.getLocalizedMessage());
+	}
+
+	@ExceptionHandler(IOException.class)
+	@ResponseBody
+	@ResponseStatus(INTERNAL_SERVER_ERROR)
+	private ExceptionDto invalidInput(IOException exception){
+		LOG.error(exception);
+		return new ExceptionDto(INTERNAL_SERVER_ERROR.value(), exception.getLocalizedMessage());
+	}
+
+
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<String> createUser(@RequestBody String userInfo) {
-		try {
-			// getting userDto from json
-			UserDto user = getUserDtoFromJson(userInfo);
-			// getting password
-			String password = mapper.readValue(userInfo, ObjectNode.class).get("password").asText();
-			userService.createUser(user, password, getCurrentUserEmail());
-			return new ResponseEntity<>("", HttpStatus.OK);
-		} catch (SnapdirectorException e) {
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-		} catch (IOException e) {
-			return new ResponseEntity<>("Failed to parse json", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+	public ResponseEntity<String> createUser(@RequestBody String userInfo) throws IOException {
+		// getting userDto from json
+		UserDto user = getUserDtoFromJson(userInfo);
+		// getting password
+		String password = mapper.readValue(userInfo, ObjectNode.class).get("password").asText();
+		userService.createUser(user, password, getCurrentUserEmail());
+		return new ResponseEntity<>("", HttpStatus.OK);
 	}
 
 	@RequestMapping(method = RequestMethod.PUT)
-	public ResponseEntity updateUser(@RequestBody String userInfo) {
-		try {
-			// getting userDto from json
-			UserDto user = getUserDtoFromJson(userInfo);
+	public ResponseEntity updateUser(@RequestBody String userInfo) throws IOException {
+		// getting userDto from json
+		UserDto user = getUserDtoFromJson(userInfo);
 
-			// getting password
-			String password = mapper.readValue(userInfo, ObjectNode.class).get("password").asText();
+		// getting password
+		String password = mapper.readValue(userInfo, ObjectNode.class).get("password").asText();
 
-			userService.updateUser(user, password, getCurrentUserEmail());
-			return new ResponseEntity<>("", HttpStatus.OK);
-		} catch (SnapdirectorException e) {
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-		} catch (IOException e) {
-			return new ResponseEntity<>("Failed to parse json", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		userService.updateUser(user, password, getCurrentUserEmail());
+		return new ResponseEntity<>("", HttpStatus.OK);
 	}
 
 
@@ -80,12 +102,8 @@ public class UserController {
 
 	@RequestMapping(value = "/{userEmail:.+}", method = RequestMethod.DELETE)
 	public ResponseEntity removeUser(@PathVariable("userEmail") String userEmail) {
-		try {
-			userService.removeUser(userEmail, getCurrentUserEmail());
-			return new ResponseEntity<>("", HttpStatus.OK);
-		} catch (SnapdirectorException e) {
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		userService.removeUser(userEmail, getCurrentUserEmail());
+		return new ResponseEntity<>("", HttpStatus.OK);
 	}
 
 
