694e4a8154a6ee73a815fc07a79ed75095db16a8
Dmitriy Vasiliev <dvas@softserveinc.com>
2015-09-02 17:36:23 +0300

    Created SnapshotDto and SnapshotDtoConverter

---
+++ src/com/sungardas/snapdirector/dto/SnapshotDto.java
@@ -0,0 +1,29 @@
+package com.sungardas.snapdirector.dto;
+
+public class SnapshotDto {
+
+	private String id;
+	private String snapshotId;
+	private String volumeId;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSnapshotId() {
+		return snapshotId;
+	}
+	public void setSnapshotId(String snapshotId) {
+		this.snapshotId = snapshotId;
+	}
+	public String getVolumeId() {
+		return volumeId;
+	}
+	public void setVolumeId(String volumeId) {
+		this.volumeId = volumeId;
+	}
+	
+	
+}
---
+++ src/com/sungardas/snapdirector/dto/converter/SnapshotDtoConverter.java
@@ -0,0 +1,43 @@
+package com.sungardas.snapdirector.dto.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+
+import com.sungardas.snapdirector.aws.dynamodb.model.Snapshot;
+import com.sungardas.snapdirector.dto.SnapshotDto;
+
+public class SnapshotDtoConverter {
+
+	public static Snapshot convert(SnapshotDto snapshotDto){
+		Snapshot snapshot = new Snapshot();
+		BeanUtils.copyProperties(snapshotDto, snapshot);
+		return snapshot;
+	}
+	
+	public static SnapshotDto convert(Snapshot snapshot){
+		SnapshotDto snapshotDto = new SnapshotDto();
+		BeanUtils.copyProperties(snapshot, snapshotDto);
+		return snapshotDto;
+	}
+	
+	/*public static List<Snapshot> convert(List<SnapshotDto> snapshotDtos){
+		
+		List<Snapshot> snapshots = new ArrayList<Snapshot>();		
+		
+		for (SnapshotDto snapshotDto : snapshotDtos) {
+			snapshots.add(SnapshotDtoConverter.convert(snapshotDto));
+		}
+		return snapshots;
+	}
+	
+	public static List<SnapshotDto> convert(List<Snapshot> snapshots) {
+		List<SnapshotDto> snapshotDtos = new ArrayList<SnapshotDto>();
+		for (Snapshot snapshot : snapshots) {
+			snapshotDtos.add(SnapshotDtoConverter.convert(snapshot));
+		}
+		return snapshotDtos;
+	}*/
+	
+}
