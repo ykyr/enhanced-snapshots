de8e6a853759971dbbfcb1f43728822686ef12fe
Dmitriy Vasiliev <dvas@softserveinc.com>
2015-09-04 17:59:13 +0300

    Added EnabledScan annotation

--- src/com/sungardas/snapdirector/aws/dynamodb/repository/SnapshotRepository.java
+++ src/com/sungardas/snapdirector/aws/dynamodb/repository/SnapshotRepository.java
@@ -2,10 +2,12 @@ package com.sungardas.snapdirector.aws.dynamodb.repository;
 
 import java.util.List;
 
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
 import com.sungardas.snapdirector.aws.dynamodb.model.Snapshot;
 
+@EnableScan
 public interface SnapshotRepository extends CrudRepository<Snapshot, String> {
 
 	List<Snapshot> findBySnapshotId(String snapshotId);
