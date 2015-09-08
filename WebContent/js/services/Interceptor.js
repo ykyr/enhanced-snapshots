59df6cb5349e6c35459aebdcb21b3adeb2170a94
vkozach <vkozach@softserveinc.com>
2015-09-08 14:34:12 +0300

    UI: Removed unused services, directives and controller. Minor bug fix and improvements

--- WebContent/js/services/Interceptor.js
+++ WebContent/js/services/Interceptor.js
@@ -3,17 +3,16 @@
  */
 
 angular.module('web')
-    .factory('Interceptor', function ($q) {
+    .factory('Interceptor', function ($q, Exception) {
 
         return {
             responseError: function (rejection) {
                 if (rejection.status === 401) {
                     window.location = "#/login";
-                } else if (rejection.status === 404) {
-
+                } else if (rejection.status === 500 && rejection.data.localizedMessage){
+                    Exception.handle(rejection);
                 }
                 return $q.reject(rejection);
             }
         }
-
     });
