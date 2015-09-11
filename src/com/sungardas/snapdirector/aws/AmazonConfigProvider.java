00d8f4f95f8d392b850e82cdf4a1b15f35902d89
Oleksii Goralik <ogor@softservecom.com>
2015-09-11 11:35:55 +0300

    [SNAP-210] : fixed

--- src/com/sungardas/snapdirector/aws/AmazonConfigProvider.java
+++ src/com/sungardas/snapdirector/aws/AmazonConfigProvider.java
@@ -71,7 +71,6 @@ public class AmazonConfigProvider {
     @Bean
     public AmazonS3 amazonS3() {
         AmazonS3 amazonS3  = new AmazonS3Client(amazonAWSCredentials());
-        amazonS3.setRegion(Region.getRegion(Regions.fromName(region)));
         return amazonS3;
     }
 
