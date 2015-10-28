33a6df2cf2a0bc932bcdac3e052e289f2ae90ddb
vkozach <vkozach@softserveinc.com>
2015-10-29 01:08:49 +0200

    SNAP-251 Schedule status not refreshing automatically

--- WebContent/js/services/Volumes.js
+++ WebContent/js/services/Volumes.js
@@ -3,35 +3,16 @@
 angular.module('web')
     .service('Volumes', function ($q, $http, Storage, BASE_URL) {
         var url = BASE_URL + 'rest/volume';
-        var storageKey = 'volumes';
 
         return {
             get: function () {
                 var deferred = $q.defer();
-                if (Storage.get(storageKey)) {
-                    deferred.resolve(Storage.get(storageKey));
-                } else {
-                    $http.get(url).then(function (result) {
-                        var data = result.data;
-                        Storage.save(storageKey, data);
-                        deferred.resolve(data);
-                    }, function (data, status) {
-                        deferred.reject(data, status)
-                    });
-                }
-                return deferred.promise;
-            },
-            refresh: function() {
-                var deferred = $q.defer();
-                Storage.remove(storageKey);
                 $http.get(url).then(function (result) {
                     var data = result.data;
-                    Storage.save(storageKey, data);
                     deferred.resolve(data);
-                }, function(msg){
-                    // TODO: handle 401 here
+                }, function (data, status) {
+                    deferred.reject(data, status)
                 });
-
                 return deferred.promise;
             }
         }
