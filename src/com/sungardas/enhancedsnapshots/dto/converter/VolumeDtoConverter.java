fa899523e610e3d1386361dcff38aa7a1d5c1e87
Kostiantyn Glukhenko <khluk@softserveinc.com>
2015-10-30 12:31:44 +0200

    SNAP-223 Document that the green boxes in the volume list are AWS tags

--- src/com/sungardas/enhancedsnapshots/dto/converter/VolumeDtoConverter.java
+++ src/com/sungardas/enhancedsnapshots/dto/converter/VolumeDtoConverter.java
@@ -5,7 +5,9 @@ import com.amazonaws.services.ec2.model.Volume;
 import com.sungardas.enhancedsnapshots.dto.VolumeDto;
 import org.springframework.beans.BeanUtils;
 
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 public class VolumeDtoConverter {
@@ -13,12 +15,16 @@ public class VolumeDtoConverter {
     public static VolumeDto convert(Volume volume) {
         VolumeDto volumeDto = new VolumeDto();
         BeanUtils.copyProperties(volume, volumeDto);
+        List<Tag> tags = new ArrayList<>();
         for (Tag tag : volume.getTags()) {
             // check whether volume has Name tag
             if (tag.getKey().equals("Name")) {
                 volumeDto.setVolumeName(tag.getValue());
+            } else {
+                tags.add(tag);
             }
         }
+        volumeDto.setTags(tags);
         // check whether volume is attached to instance
         if (volume.getAttachments().size() > 0) {
             volumeDto.setInstanceID(volume.getAttachments().get(0).getInstanceId());
